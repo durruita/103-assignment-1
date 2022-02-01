a10e23d59c22ee5b264060b18cded0dc17d57ade
durruita <97204184+durruita@users.noreply.github.com>
2022-01-31 20:44:38 -0800

    Add files via upload

---
+++ var/var.js
@@ -0,0 +1,54 @@
+let customer;
+customer="Trent";
+
+let customer2;
+customer2="Michelle";
+
+let qty;
+qty="1"
+
+let qty2;
+qty2="2"
+
+let size;
+size="small"
+
+let size2;
+size2="large"
+
+let item;
+item="tea";
+
+let item2;
+item2="coffee";
+
+let item3;
+item3="sugar";
+
+let item4;
+item4="no sugar";
+
+let card;
+card="card"
+
+let cash;
+cash="cash"
+
+document.write(`${customer} ordered ${qty} ${size} ${item} with  ${item3} he paid with  ${card}.`)
+
+document.write(` ${customer2} ordered ${qty2} ${size2} ${item2} with  ${item4} he paid with  ${cash}.`)
+
+let win;
+win="4";
+
+let door;
+door="5"
+
+let room;
+room="5"
+
+let bath=2;
+
+let fans=3;
+
+document.write(`My apartment has ${win} windows,  ${door} doors,  ${room} rooms, ${bath} bathrooms, and  ${fan} fans.`)
